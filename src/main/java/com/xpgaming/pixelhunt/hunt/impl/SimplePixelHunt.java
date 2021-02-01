b99cb60528ebe8c264305f3e4e35a21f1e8ae23a
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-01 22:07:22 +0000

    Started SimplePixelHunt implementation

---
+++ src/main/java/com/xpgaming/pixelhunt/hunt/impl/SimplePixelHunt.java
@@ -0,0 +1,57 @@
+package com.xpgaming.pixelhunt.hunt.impl;
+
+import com.google.common.collect.Lists;
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+import com.pixelmonmod.pixelmon.api.pokemon.PokemonSpec;
+import com.xpgaming.pixelhunt.hunt.PixelHunt;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.configurate.ConfigurationNode;
+
+import java.util.List;
+
+public class SimplePixelHunt implements PixelHunt {
+
+    private final String identifier;
+    private final List<String> rewardCommands = Lists.newArrayList();
+
+    private ItemStack displayItem;
+    private PokemonSpec currentPokemon;
+    private boolean randomCommands;
+
+    public SimplePixelHunt(String identifier, ConfigurationNode node) {
+        this.identifier = identifier;
+
+        this.load(node);
+    }
+
+    @Override
+    public String getIdentifier() {
+        return this.identifier;
+    }
+
+    @Override
+    public void load(ConfigurationNode config) {
+
+    }
+
+    @Override
+    public ItemStack getDisplay() {
+        return this.displayItem;
+    }
+
+    @Override
+    public PokemonSpec generatePokemon() {
+        return null; //TODO:
+    }
+
+    @Override
+    public boolean isBeingHunted(Pokemon pokemon) {
+        return this.currentPokemon.matches(pokemon);
+    }
+
+    @Override
+    public void rewardCatch(EntityPlayerMP player) {
+        //TODO:
+    }
+}
