4759be1f5ed72cd9dbbbabf8adafc76aad2659db
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-01 22:03:38 +0000

    Started PixelHunt interface

---
+++ src/main/java/com/xpgaming/pixelhunt/hunt/PixelHunt.java
@@ -0,0 +1,56 @@
+package com.xpgaming.pixelhunt.hunt;
+
+import com.pixelmonmod.pixelmon.api.pokemon.Pokemon;
+import com.pixelmonmod.pixelmon.api.pokemon.PokemonSpec;
+import net.minecraft.entity.player.EntityPlayerMP;
+import org.spongepowered.configurate.ConfigurationNode;
+
+/**
+ *
+ * An interface to represent the different hunts available on a server
+ *
+ */
+public interface PixelHunt {
+
+    /**
+     *
+     * Identifier of the hunt rarity
+     *
+     * @return The hunt's identifier
+     */
+    String getIdentifier();
+
+    /**
+     *
+     * Loads the hunt from the {@link ConfigurationNode}
+     *
+     * @param config The node the hunt is being loaded from
+     */
+    void load(ConfigurationNode config);
+
+    /**
+     *
+     * Randomly generates the next pokemon for this hunt's rarity.
+     *
+     * @return The specification of the new pokemon to be hunted
+     */
+    PokemonSpec generatePokemon();
+
+    /**
+     *
+     * Checks if a specific pokemon is currently the target for this hunt
+     *
+     * @param pokemon The target pokemon
+     * @return if it's to be hunted or not
+     */
+    boolean isBeingHunted(Pokemon pokemon);
+
+    /**
+     *
+     * Rewards the player for catching the hunted pokemon
+     *
+     * @param player The player who won the hunt
+     */
+    void rewardCatch(EntityPlayerMP player);
+
+}
