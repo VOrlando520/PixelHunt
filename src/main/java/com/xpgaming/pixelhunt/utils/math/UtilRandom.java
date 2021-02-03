7c499f74d3938501684b6b0c04c11fcf3ee66a71
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-03 23:17:00 +0000

    Added UtilRandom

---
+++ src/main/java/com/xpgaming/pixelhunt/utils/math/UtilRandom.java
@@ -0,0 +1,24 @@
+package com.xpgaming.pixelhunt.utils.math;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class UtilRandom {
+
+    public static <T> T getRandomElementExcluding(T[] array, T... exclude) {
+        T randomElement = getRandomElement(array);
+        List<T> exclusions = Arrays.asList(exclude);
+
+        while (exclusions.contains(randomElement)) {
+            randomElement = getRandomElement(array);
+        }
+
+        return randomElement;
+    }
+
+    public static <T> T getRandomElement(T[] array) {
+        return array[ThreadLocalRandom.current().nextInt(array.length)];
+    }
+
+}
