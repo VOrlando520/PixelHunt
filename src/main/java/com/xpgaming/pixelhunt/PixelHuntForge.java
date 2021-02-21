96a22081ea9beaa409f5beb17dc2dcb7ac4dabcd
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-02-21 23:01:08 +0000

    Registered type injector

--- src/main/java/com/xpgaming/pixelhunt/PixelHuntForge.java
+++ src/main/java/com/xpgaming/pixelhunt/PixelHuntForge.java
@@ -5,11 +5,14 @@ import com.envyful.acaf.impl.ForgeCommandFactory;
 import com.pixelmonmod.pixelmon.Pixelmon;
 import com.xpgaming.pixelhunt.commands.PixelHuntCommand;
 import com.xpgaming.pixelhunt.config.PixelHuntConfig;
+import com.xpgaming.pixelhunt.hunt.PixelHunt;
 import com.xpgaming.pixelhunt.hunt.PixelHuntFactory;
 import com.xpgaming.pixelhunt.listener.PokemonCaptureListener;
 import com.xpgaming.pixelhunt.listener.PokemonSpawnListener;
 import com.xpgaming.pixelhunt.task.ParticleDisplayTask;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
@@ -32,6 +35,8 @@ public class PixelHuntForge {
     public static final String MOD_ID = "pixelhunt";
     public static final String MOD_NAME = "PixelHunt Forge";
     public static final String MOD_VERSION = "1.12.2-forge-1.0.1";
+
+    private static final ITextComponent NO_SUCH_HUNT = new TextComponentString("Cannot find a PokeHunt by that id");
 
     private static PixelHuntForge instance;
     private static MinecraftServer server;
@@ -57,6 +62,21 @@ public class PixelHuntForge {
         MinecraftForge.EVENT_BUS.register(new PokemonSpawnListener(this));
         Pixelmon.EVENT_BUS.register(new PokemonCaptureListener(this));
 
+        this.commandFactory.registerInjector(PixelHunt.class, (sender, args) -> {
+            if (args.length != 1) {
+                sender.sendMessage(NO_SUCH_HUNT);
+                return null;
+            }
+
+            PixelHunt hunt = PixelHuntFactory.getHunt(args[0]);
+
+            if (hunt == null) {
+                sender.sendMessage(NO_SUCH_HUNT);
+                return null;
+            }
+
+            return hunt;
+        });
         this.commandFactory.registerCommand(server, new PixelHuntCommand());
     }
 
